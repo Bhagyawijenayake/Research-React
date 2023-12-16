9c5532954b9dae6f35bf42d92f3cb1ea67bbc787
Bhagya Wijenayake <bhagyasudaraka98@gmail.com>
2023-12-17 00:40:13 +0530

    add alt to logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import logo from '/logo.png';
 const Nav = () => {
     return (
         <nav>
-                <img src={logo} alt="" />
+                <img src={logo} alt="logo" />
                 <h2>Bhagya Wijenayake</h2>
         </nav>
     )
